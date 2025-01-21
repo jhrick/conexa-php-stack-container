97c341325d8b14ab0c322eb4b5aea7fcd2ea2e64
jhrick <hj.rickcp@gmail.com>
2025-01-21 09:35:22 -0300

    chore: get dynamic values

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-docker run \
+## run bash inside container
+read -p "Enter container name: " name
+
+image=$(docker ps -a --format "{{.Image}}" --filter "name=^/${name}$")
+
+if [ -z "$image" ]; then
+  echo "No container found with the name: $name"
+else
+  docker run \
   -v ./src:/var/www/html/WebRoot \
-  -it yii-docker-server \
+  -it $image \
   bash
+fi
---
+++ yii.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p "Enter project name: " app_name
+
+php framework/yiic.php webapp WebRoot/$app_name
